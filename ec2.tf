54ee1e39f15645352295392bab9410ecdfb0cf98
groca-sysdig <105215194+groca-sysdig@users.noreply.github.com>
2023-10-04 15:46:03 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -88,8 +88,8 @@ resource "aws_security_group" "web-node" {
     "0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    to_port   = 22
+    from_port = 0
+    to_port   = 0
     protocol  = "tcp"
     cidr_blocks = [
     "0.0.0.0/0"]

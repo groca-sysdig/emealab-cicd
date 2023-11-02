5d16329227130371b1ffc7421f3de9fddfe6ddb2
groca-sysdig <105215194+groca-sysdig@users.noreply.github.com>
2023-11-02 18:22:40 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -81,8 +81,8 @@ resource "aws_security_group" "web-node" {
   vpc_id      = aws_vpc.web_vpc.id
 
   ingress {
-    from_port = 80
-    to_port   = 80
+    from_port = 0
+    to_port   = 0
     protocol  = "tcp"
     cidr_blocks = [
     "0.0.0.0/0"]
